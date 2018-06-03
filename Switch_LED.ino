07cc833e5b3c5abf3e887c4cd9276b692c44de33
byronprice <byron.h.price@gmail.com>
2018-06-03 14:15:49 -0400

    add arduino code

---
+++ Switch_LED.ino
@@ -0,0 +1,24 @@
+const int switchPin = 2;
+const int ledPin = 10;
+int switchState = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin,OUTPUT);
+  pinMode(switchPin,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  switchState = digitalRead(switchPin);
+
+  if (switchState == HIGH) {
+    digitalWrite(ledPin,HIGH);
+    delay(1000);
+    digitalWrite(ledPin,LOW);
+    delay(1000);
+  }
+  else {
+    digitalWrite(ledPin,LOW);
+  }
+  }
